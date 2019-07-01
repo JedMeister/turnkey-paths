505b92af2e2680af10feb51c316655f527f8716a
Stefan Davis <stefan@turnkeylinux.org>
2019-07-01 03:19:29 +0000

    Migrated from pylib

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='turnkey-paths',
+    version='1.0',
+    description='Python high level utility for working with paths',
+    author='Stefan Davis',
+    url='https://github.com/turnkeylinux/turnkey-paths',
+    packages=['paths'])
